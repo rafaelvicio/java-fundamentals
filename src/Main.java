38385ddaac4ebe497f15c10fb0e359855ac8e46d
Rafael Augusto Pacheco Baião Reis <rafaelvicio@icloud.com>
2017-06-19 15:56:35 -0300

    hello Word + package

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by STEFANINI on 19/06/2017.
- */
-public class Main {
-
-    public static void amin(String[] args){
-
-    }
-}
---
+++ src/br/com/rafaeldev/fundamentals/Main.java
@@ -0,0 +1,13 @@
+package br.com.rafaeldev.fundamentals;
+
+/**
+ * Created by STEFANINI on 19/06/2017.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+
+    }
+}
